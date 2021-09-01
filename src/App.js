10ee12b24064ab2b0ab1ca75584c1bd99a658ae8
vinicius <vinapro91@gmail.com>
2021-09-01 12:12:01 -0300

    feat: criado funcionalidade de gerar numeros aleatorios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(Math.floor(Math.random() * 100) + 1);
+
+  function randomNumber() {
+    setNumber(Math.floor(Math.random() * 100) + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Number">{number}</div>
+      <button onClick={() => randomNumber()}> Criar novo número</button>
     </div>
   );
 }
